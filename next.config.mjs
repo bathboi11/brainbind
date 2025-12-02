559217784f969c66129f900d668eaf8d3a59ba28
bathboi11 <75325031+bathboi11@users.noreply.github.com>
2025-12-02 18:12:57 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,15 @@
-import { withPWA } from '@ducanh2912/next-pwa';
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+  },
+};
 
-const withPWAConfig = withPWA({
+const withPWA = require('@ducanh2912/next-pwa').default({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
   register: true,
   skipWaiting: true,
-});
+}).withNextConfig;
 
-export default withPWAConfig({
-  experimental: { serverActions: true },
-});
+module.exports = withPWA(nextConfig);
