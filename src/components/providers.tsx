36ca7858d1d53f382fe3c47281473de41e72d5bd
bathboi11 <75325031+bathboi11@users.noreply.github.com>
2025-12-03 20:46:06 -0500

    Create providers.tsx

---
+++ src/components/providers.tsx
@@ -0,0 +1,14 @@
+// src/components/providers.tsx
+'use client';
+
+import { SessionProvider } from 'next-auth/react';
+
+export default function Providers({
+  children,
+  session,
+}: {
+  children: React.ReactNode;
+  session: any;
+}) {
+  return <SessionProvider session={session}>{children}</SessionProvider>;
+}
