25d734611bc4d339f4db7cf7b76077930a018ee8
bathboi11 <75325031+bathboi11@users.noreply.github.com>
2025-12-03 20:19:38 -0500

    Update stripe.ts

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,4 +1,6 @@
+// src/lib/stripe.ts
 import Stripe from 'stripe';
+
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-06-20',
+  apiVersion: '2025-11-17', // ← latest version as of Dec 2025
 });
