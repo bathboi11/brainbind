c880792a2290e3d70c11fa8f5b402965f2698718
bathboi11 <75325031+bathboi11@users.noreply.github.com>
2026-01-20 22:08:14 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
+const withPWA = require('@ducanh2912/next-pwa').default({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    return config;
+  },
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
